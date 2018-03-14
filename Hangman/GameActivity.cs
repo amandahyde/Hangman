9ed0c099d7dde6808ac562b76c8d3aa7321ddabe
amandahyde <amandahyde84@gmail.com>
2018-03-14 14:24:35 +1300

    Add project files.

---
+++ Hangman/GameActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Hangman
+{
+    [Activity(Label = "GameActivity")]
+    public class GameActivity : Activity
+    {
+        private TextView txtMessage;
+        private String Name;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.Game);
+
+            Name = Intent.GetStringExtra("Name");
+
+            txtMessage = FindViewById<TextView>(Resource.Id.txtWelcome);
+            txtMessage.Text = "Welcome to the game " + Name;
+
+        }
+    }
+}
---
+++ Hangman/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Content;
+using System;
+
+namespace Hangman
+{
+    [Activity(Label = "Hangman", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+       
+        private Button btnNext;
+        private EditText EnterName;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+       
+        }
+
+        private void Startup() {
+
+            EnterName = FindViewById<EditText>(Resource.Id.ETEnterName);
+            btnNext = FindViewById<Button>(Resource.Id.btnNext);
+
+            btnNext.Click += onNext_Click;
+
+
+        }
+
+        private void onNext_Click(object sender, EventArgs e)
+        {
+            //toast check to see its working
+            Toast.MakeText(this, "Your name is " + EnterName.Text, ToastLength.Long).Show();
+
+            //create an intent to move data to the other activity.
+            var gameActivity = new Intent(this, typeof(GameActivity));
+            gameActivity.PutExtra("Name", EnterName.Text);
+
+            //start game activity
+            StartActivity(typeof(GameActivity));
+        }
+        }
+}
+
